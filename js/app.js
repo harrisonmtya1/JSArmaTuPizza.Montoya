af6610de0de4bcebc97eac73a57205c2a4d607cf
Harrison Montoya <harrimv206@gmail.com>
2021-07-17 15:32:02 -0500

    Primer commit en Rama Objetos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,50 @@
+class cliente{
+
+    constructor(nombre,telefono,direccion) {
+        this.nombre=nombre;
+        this.telefono=telefono;
+        this.direccion=direccion;
+    }
+
+    solicitarDatos(){
+        this.nombre=prompt("Por favor ingrese su nombre");
+        this.telefono=prompt("Telefono de contacto");
+        this.direccion=prompt("Direccion de domicilio");
+        alert("Los Datos ingresados son:" +"\nNombre: "+this.nombre+"\nTelefono: "+this.telefono+"\nDireccion: "+this.direccion);
+    }
+
+    
+
+}
+
+class pizza{
+
+    constructor(tamaño,numeroIngredientes){
+        this.tamaño=tamaño;
+        this.numeroIngredientes=numeroIngredientes;
+    }
+
+    armarPizza(){
+        this.tamaño=prompt("Ingrese el tamaño de su pizza\n"+"Escoja entre las siguientes opciones: familiar, mediana, personal");
+        this.numeroIngredientes=prompt("Ingrese la cantidad de ingredientes que desea: 1, 2, 3, 4 o 5");
+    }
+
+    calcuparPrecio(tamañoPizza,numeroIngredientes){
+        switch(tamañoPizza){
+            case "familiar":
+                return numeroIngredientes * 3000 + 20000;
+            case "mediana":
+                return numeroIngredientes * 2000 + 15000;
+            case "personal":
+                return numeroIngredientes * 1000 + 10000;
+            default:
+                return 45000;
+            
+        }
+    }
+    
+}
+
 let nombreCliente="";
 let telefonoCliente="";
 let direccionCliente="";
@@ -5,39 +52,22 @@ let numeroIngredientes=1;
 let tamañoPizza="";
 let precioFinal=0;
 
+let objPizza=new pizza(tamañoPizza,numeroIngredientes);
+let objCliente=new cliente(nombreCliente,telefonoCliente,direccionCliente);
+
 alert("Bienvenido a la Pizzeria!!" +"\nA continuacion ingresa tus Datos para inciar el proceso de compra");
 
-ingresarDatosCliente();
-armarPizza();
-precioFinal=calcuparPrecio(tamañoPizza,numeroIngredientes);
+objCliente.solicitarDatos();
+objPizza.armarPizza();
+precioFinal=objPizza.calcuparPrecio(objPizza.tamaño,objPizza.numeroIngredientes);
 alert("El costo de su pedido es " + precioFinal + " e incluye el domicilio");
 
 
-function ingresarDatosCliente(){
-    nombreCliente=prompt("Por favor ingrese su nombre");
-    telefonoCliente=prompt("Telefono de contacto");
-    direccionCliente=prompt("Direccion de domicilio");
-    alert("Los Datos ingresados son:" +"\nNombre: "+nombreCliente+"\nTelefono: "+telefonoCliente+"\nDireccion: "+direccionCliente);
-}
 
-function armarPizza(){
-   tamañoPizza=prompt("Ingrese el tamaño de su pizza\n"+"Escoja entre las siguientes opciones: familiar, mediana, personal");
-   numeroIngredientes=prompt("Ingrese la cantidad de ingredientes que desea: 1, 2, 3, 4 o 5");
-    
-}
 
-function calcuparPrecio(tamañoPizza,numeroIngredientes){
-    switch(tamañoPizza){
-        case "familiar":
-            return numeroIngredientes * 3000 + 20000;
-        case "mediana":
-            return numeroIngredientes * 2000 + 15000;
-        case "personal":
-            return numeroIngredientes * 1000 + 10000;
-        default:
-            return 45000;
-        
-    }
 
-}
+
+
+
+
 
