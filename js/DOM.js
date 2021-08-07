b54dc130493d3cda0179a2caeafa3d4bc5dbd77e
Harrison Montoya <harrimv206@gmail.com>
2021-08-06 20:53:39 -0500

    Se agrega eventos a boton de ordenar en formulario de carta

--- js/DOM.js
+++ js/DOM.js
@@ -23,23 +23,69 @@ let carta = [{
     precio: [7500, 9800, 13000]
 }]
 
-let tablaBody=document.getElementById("tablaBody")
-let seleccionPizza=document.querySelector("#seleccionPizza");
+document.getElementById("btnConfirmarCompra").style.display = "none";
+
+// codigo para llenar tabla con informacion de la carta de pizzas y menu desplegable - seléction
+let tablaBody = document.getElementById("tablaBody")
+let seleccionPizza = document.querySelector("#seleccionPizza");
 
 for (pizza of carta) {
     let nuevaFila = document.createElement("tr");
-    let nuevaOpcion=document.createElement("option");
+    let nuevaOpcion = document.createElement("option");
 
     nuevaFila.innerHTML = `<td> ${pizza.nombre}</td>
     <td>${pizza.descripcion}</td>
     <td>${pizza.precio[0]}</td>   
     <td>${pizza.precio[1]}</td>
-    <td>${pizza.precio[2]}</td> `; 
+    <td>${pizza.precio[2]}</td> `;
     tablaBody.appendChild(nuevaFila);
 
-    nuevaOpcion.innerHTML=`${pizza.nombre}`;
+    nuevaOpcion.innerHTML = `${pizza.nombre}`;
     seleccionPizza.appendChild(nuevaOpcion);
 }
 
+// codigo que añade pizza en la seccion de confirmacion del pedido
 
+let formOrdenarCarta = document.getElementById("ordenarCarta");
+
+formOrdenarCarta.addEventListener("submit", ordenarCarta);
+
+function ordenarCarta(e) {
+    var precioTotal;
+    e.preventDefault();
+    var datosFormulario= e.target;
+    nuevoDiv=document.createElement("div");
+
+    nuevoDiv.setAttribute("class","container");
+    nuevoDiv.setAttribute("class","row");
+
+    nuevoDiv.innerHTML=`<div class="col-md-2">${datosFormulario.children[0].value}</div>`;
+    for(pizza of carta){
+        if(pizza.nombre== datosFormulario.children[0].value){
+            nuevoDiv.innerHTML+=`<div class="col-md-4">${pizza.descripcion}</div>`;
+            debugger
+            if(datosFormulario.children[2].value==""){
+                datosFormulario.children[2].value=1;
+            }
+            switch(datosFormulario.children[1].value){
+              case "Personal":
+                  precioTotal=datosFormulario.children[2].value * pizza.precio[0];
+              break;
+              case "Mediana":
+                precioTotal=datosFormulario.children[2].value * pizza.precio[1];
+              break;
+              case "Familiar":
+                precioTotal=datosFormulario.children[2].value * pizza.precio[2];
+              break;
+            }  
+        }
+    }    
+    nuevoDiv.innerHTML+=`<div class="col-md-2">${datosFormulario.children[1].value}</div>`;
+    nuevoDiv.innerHTML+=`<div class="col-md-2">${datosFormulario.children[2].value}</div>`; 
+    nuevoDiv.innerHTML+=`<div class="col-md-2">${precioTotal}</div>`;
+
+    
+    document.getElementById("listaPedidos").insertBefore(nuevoDiv,document.getElementById("listaPedidos").getElementsByTagName("button")[0]);
+    document.getElementById("btnConfirmarCompra").style.display = "block";
+}
 
