93c2237ed5426d59bc4745636268659c1463b7a5
Harrison Montoya <harrimv206@gmail.com>
2021-08-14 15:54:32 -0500

    Implementacion de localStore

--- js/DOM.js
+++ js/DOM.js
@@ -1,3 +1,17 @@
+class pizzasConfirmadas {
+
+    constructor(nombre, descripcion, tamaño, cantidad, precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.tamaño = tamaño;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+}
+
+
+let arrayPizzasConfirmadas = [];
+
 let carta = [{
     nombre: "Americana",
     descripcion: "Jamon ahumado, salami y doblequeso",
@@ -25,13 +39,14 @@ let carta = [{
 
 document.getElementById("btnConfirmarCompra").style.display = "none";
 
-// codigo para llenar tabla con informacion de la carta de pizzas y menu desplegable - seléction
+// codigo para llenar tabla con informacion de la carta de pizza y menu desplegable - seléction
 let tablaBody = document.getElementById("tablaBody")
-let seleccionPizza = document.querySelector("#seleccionPizza");
+let seleccionPizza = document.querySelectorAll(".seleccionPizza");
 
 for (pizza of carta) {
     let nuevaFila = document.createElement("tr");
-    let nuevaOpcion = document.createElement("option");
+    let nuevaOpcion1 = document.createElement("option");
+    let nuevaOpcion2 = document.createElement("option");
 
     nuevaFila.innerHTML = `<td> ${pizza.nombre}</td>
     <td>${pizza.descripcion}</td>
@@ -40,8 +55,11 @@ for (pizza of carta) {
     <td>${pizza.precio[2]}</td> `;
     tablaBody.appendChild(nuevaFila);
 
-    nuevaOpcion.innerHTML = `${pizza.nombre}`;
-    seleccionPizza.appendChild(nuevaOpcion);
+    nuevaOpcion1.innerHTML = `${pizza.nombre}`;
+    nuevaOpcion2.innerHTML = `${pizza.nombre}`;
+
+    seleccionPizza[0].appendChild(nuevaOpcion1);
+    seleccionPizza[1].appendChild(nuevaOpcion2);
 }
 
 // codigo que añade pizza en la seccion de confirmacion del pedido
@@ -51,41 +69,56 @@ let formOrdenarCarta = document.getElementById("ordenarCarta");
 formOrdenarCarta.addEventListener("submit", ordenarCarta);
 
 function ordenarCarta(e) {
+    let objPizzasConfirmadas = new pizzasConfirmadas();
     var precioTotal;
     e.preventDefault();
-    var datosFormulario= e.target;
-    nuevoDiv=document.createElement("div");
+    var datosFormulario = e.target;
+    nuevoDiv = document.createElement("div");
+    nuevoDiv.setAttribute("class", "row pizzaConfirmar");
 
-    nuevoDiv.setAttribute("class","container");
-    nuevoDiv.setAttribute("class","row");
-
-    nuevoDiv.innerHTML=`<div class="col-md-2">${datosFormulario.children[0].value}</div>`;
-    for(pizza of carta){
-        if(pizza.nombre== datosFormulario.children[0].value){
-            nuevoDiv.innerHTML+=`<div class="col-md-4">${pizza.descripcion}</div>`;
-            debugger
-            if(datosFormulario.children[2].value==""){
-                datosFormulario.children[2].value=1;
+    nuevoDiv.innerHTML = `<div class="col-md-2">${datosFormulario.children[0].value}</div>`;
+    objPizzasConfirmadas.nombre = datosFormulario.children[0].value;
+    for (pizza of carta) {
+        if (pizza.nombre == datosFormulario.children[0].value) {
+            nuevoDiv.innerHTML += `<div class="col-md-4">${pizza.descripcion}</div>`;
+            objPizzasConfirmadas.descripcion = pizza.descripcion;
+            if (datosFormulario.children[2].value == "") {
+                datosFormulario.children[2].value = 1;
+                objPizzasConfirmadas.cantidad = datosFormulario.children[2].value;
             }
-            switch(datosFormulario.children[1].value){
-              case "Personal":
-                  precioTotal=datosFormulario.children[2].value * pizza.precio[0];
-              break;
-              case "Mediana":
-                precioTotal=datosFormulario.children[2].value * pizza.precio[1];
-              break;
-              case "Familiar":
-                precioTotal=datosFormulario.children[2].value * pizza.precio[2];
-              break;
-            }  
+            switch (datosFormulario.children[1].value) {
+                case "Personal":
+                    precioTotal = datosFormulario.children[2].value * pizza.precio[0];
+                    break;
+                case "Mediana":
+                    precioTotal = datosFormulario.children[2].value * pizza.precio[1];
+                    break;
+                case "Familiar":
+                    precioTotal = datosFormulario.children[2].value * pizza.precio[2];
+                    break;
+            }
         }
-    }    
-    nuevoDiv.innerHTML+=`<div class="col-md-2">${datosFormulario.children[1].value}</div>`;
-    nuevoDiv.innerHTML+=`<div class="col-md-2">${datosFormulario.children[2].value}</div>`; 
-    nuevoDiv.innerHTML+=`<div class="col-md-2">${precioTotal}</div>`;
+    }
+    nuevoDiv.innerHTML += `<div class="col-md-2">${datosFormulario.children[1].value}</div>`;
+    objPizzasConfirmadas.tamaño = datosFormulario.children[1].value;
+    nuevoDiv.innerHTML += `<div class="col-md-2">${datosFormulario.children[2].value}</div>`;
+    objPizzasConfirmadas.cantidad = datosFormulario.children[2].value;
+    nuevoDiv.innerHTML += `<div class="col-md-2">${precioTotal}</div>`;
+    objPizzasConfirmadas.precio = precioTotal;
+    arrayPizzasConfirmadas.push(objPizzasConfirmadas);
 
-    
-    document.getElementById("listaPedidos").insertBefore(nuevoDiv,document.getElementById("listaPedidos").getElementsByTagName("button")[0]);
+    document.getElementById("listaPedidos").insertBefore(nuevoDiv, document.getElementById("listaPedidos").getElementsByTagName("button")[0]);
     document.getElementById("btnConfirmarCompra").style.display = "block";
+    window.scrollBy(0, window.innerHeight);
 }
 
+let btnConfirmarCompra = document.getElementById("btnConfirmarCompra");
+
+btnConfirmarCompra.addEventListener("click", confirmarCompra);
+
+function confirmarCompra(e) {
+    alert("Tu pedido ha sido confirmado");
+    localStorage.clear();
+      localStorage.setItem("Pizzas Confirmadas",JSON.stringify(arrayPizzasConfirmadas));
+      
+}
