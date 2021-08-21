82a1fc56adc9045c0159e232b42b047ee612be77
Harrison Montoya <harrimv206@gmail.com>
2021-08-21 17:12:34 -0500

    Se agrega variables y metodos para armar pizza personalizada

--- js/DOM.js
+++ js/DOM.js
@@ -1,3 +1,6 @@
+
+// //clse para armar objetos de pizzas a confirmar y guardar estos en un array y al momento de confirmarlos los 
+// guardamos en el localStore
 class pizzasConfirmadas {
 
     constructor(nombre, descripcion, tamaño, cantidad, precio) {
@@ -12,6 +15,7 @@ class pizzasConfirmadas {
 
 let arrayPizzasConfirmadas = [];
 
+// array con objetos pizza
 let carta = [{
     nombre: "Americana",
     descripcion: "Jamon ahumado, salami y doblequeso",
@@ -45,8 +49,7 @@ let seleccionPizza = document.querySelectorAll(".seleccionPizza");
 
 for (pizza of carta) {
     let nuevaFila = document.createElement("tr");
-    let nuevaOpcion1 = document.createElement("option");
-    let nuevaOpcion2 = document.createElement("option");
+    let nuevaOpcion = document.createElement("option");
 
     nuevaFila.innerHTML = `<td> ${pizza.nombre}</td>
     <td>${pizza.descripcion}</td>
@@ -55,11 +58,9 @@ for (pizza of carta) {
     <td>${pizza.precio[2]}</td> `;
     tablaBody.appendChild(nuevaFila);
 
-    nuevaOpcion1.innerHTML = `${pizza.nombre}`;
-    nuevaOpcion2.innerHTML = `${pizza.nombre}`;
+    nuevaOpcion.innerHTML = `${pizza.nombre}`;
+    seleccionPizza[0].appendChild(nuevaOpcion);
 
-    seleccionPizza[0].appendChild(nuevaOpcion1);
-    seleccionPizza[1].appendChild(nuevaOpcion2);
 }
 
 // codigo que añade pizza en la seccion de confirmacion del pedido
@@ -74,7 +75,7 @@ function ordenarCarta(e) {
     e.preventDefault();
     var datosFormulario = e.target;
     nuevoDiv = document.createElement("div");
-    nuevoDiv.setAttribute("class", "row pizzaConfirmar");
+    nuevoDiv.setAttribute("class", "row pizzaConfirmar pizzasPedidos__pizzas");
 
     nuevoDiv.innerHTML = `<div class="col-md-2">${datosFormulario.children[0].value}</div>`;
     objPizzasConfirmadas.nombre = datosFormulario.children[0].value;
@@ -112,6 +113,9 @@ function ordenarCarta(e) {
     window.scrollBy(0, window.innerHeight);
 }
 
+// en esta seccion de codigo confirmmos la pizzas a comprar, capturamos el boton y le agregamos evento de click
+// el array de pizzasConfirmadas lo guardamos en el localStore y eliminamos las pizzas a confirmar
+
 let btnConfirmarCompra = document.getElementById("btnConfirmarCompra");
 
 btnConfirmarCompra.addEventListener("click", confirmarCompra);
@@ -119,6 +123,112 @@ btnConfirmarCompra.addEventListener("click", confirmarCompra);
 function confirmarCompra(e) {
     alert("Tu pedido ha sido confirmado");
     localStorage.clear();
-      localStorage.setItem("Pizzas Confirmadas",JSON.stringify(arrayPizzasConfirmadas));
-      
+    localStorage.setItem("Pizzas Confirmadas", JSON.stringify(arrayPizzasConfirmadas));
+    $(".pizzasPedidos__pizzas").slideUp("slow", () => {
+        $(".pizzasPedidos__pizzas").remove();
+    });
 }
+
+// seccion de codigo donde vamos armar las pizzas
+
+let precioPizzaArmada = 7000;
+
+$(".botonIngrediente").click((e) => {
+
+    $("#armarPizza_tamaño").attr("disabled", "true");
+    $("#armarPizza_cantidad").attr("disabled", "true");
+    switch (e.target.value) {
+        case "Piña":
+            switch ($("#armarPizza_tamaño").val()) {
+                case "Personal":
+                    precioPizzaArmada += ((800) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Piña ");
+                    break;
+                case "Mediana":
+                    precioPizzaArmada += ((1300) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Piña ");
+                    break;
+                case "Familiar":
+                    precioPizzaArmada += ((1800) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Piña ");
+                    break;
+            }
+            break;
+        case "Jamon":
+            switch ($("#armarPizza_tamaño").val()) {
+                case "Personal":
+                    precioPizzaArmada += ((1100) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Jamon ");
+                    break;
+                case "Mediana":
+                    precioPizzaArmada += ((1300) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Jamon ");
+                    break;
+                case "Familiar":
+                    precioPizzaArmada += ((1500) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Jamon ");
+                    break;
+            }
+            break;
+        case "Tocineta":
+            switch ($("#armarPizza_tamaño").val()) {
+                case "Personal":
+                    precioPizzaArmada += ((1000) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Tocineta ");
+                    break;
+                case "Mediana":
+                    precioPizzaArmada += ((1500) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Tocineta ");
+                    break;
+                case "Familiar":
+                    precioPizzaArmada += ((2000) * ($("#armarPizza_cantidad").val()));
+                    $("#tdPrecio").text(precioPizzaArmada);
+                    $("#tdIngredientes").text($("#tdIngredientes").text() + "Tocineta ");
+                    break;
+            }
+            break;
+    }
+
+
+})
+
+$("#armarPizza_tamaño").change((e) => {
+    $("#tdTamaño").text(e.target.value);
+    switch (e.target.value) {
+        case "Personal":
+            precioPizzaArmada = 7000;
+            break;
+        case "Mediana":
+            precioPizzaArmada = 8000;
+            break;
+        case "Familiar":
+            precioPizzaArmada = 10000;
+            break;
+    }
+    console.log(precioPizzaArmada);
+}).change();
+
+$("#armarPizza_cantidad").change((e) => {
+    $("#tdCantidad").text(e.target.value);
+    precioPizzaArmada *= (e.target.value);
+    console.log(precioPizzaArmada);
+})
+
+$("#btnArmarDeNuevo").click(() => {
+    $("#armarPizza_tamaño").removeAttr("disabled");
+    $("#armarPizza_cantidad").removeAttr("disabled");
+    $("#armarPizza_cantidad").val(1);
+    $("#tdPrecio").text(0);
+    $("#tdCantidad").text(1);
+    $("#tdIngredientes").text("");
+    precioPizzaArmada = 7000;
+});
+
